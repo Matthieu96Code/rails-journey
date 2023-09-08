7b14cb10f2e9be72f6204017fc3673303626e7bd
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-08 23:57:28 +0000

    Fix linters error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
+  def salut
+    @name = params[:name]
+  end
 
-    def salut
-        @name = params[:name]
-    end
-    
-    def home
-    end
-
+  def home; end
 end
