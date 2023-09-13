191d9b6f02867e1756a52378ef297808cf934881
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-13 15:47:41 +0000

    Fix linters error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
@@ -21,12 +20,12 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-  
+
   def create
     post = Post.create(post_params)
     redirect_to post_path(post.id)
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
@@ -38,5 +37,4 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:name, :content)
   end
-
 end
