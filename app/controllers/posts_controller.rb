8cdccb0e09633470b40ebc0ba052e3c17338298b
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-13 15:33:38 +0000

    Add post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,41 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    post = Post.create(post_params)
+    redirect_to post_path(post.id)
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :content)
+  end
+
 end
